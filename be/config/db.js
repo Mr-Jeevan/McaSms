581943b256639e489f47fb313b100709c07decc0
Mr-Jeevan <154218922+Mr-Jeevan@users.noreply.github.com>
2025-08-17 20:01:00 +0530

    Update db.js

--- be/config/db.js
+++ be/config/db.js
@@ -4,11 +4,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/srms', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+   try {
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/srms')
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection error:', err);
