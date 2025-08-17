06c5572662efcebb7e14b07ba538ffc83c822c8a
Mr-Jeevan <jeevee.a77@gmail.com>
2025-08-17 15:17:58 +0530

    added render link to the apiconfig.js file in the fe

--- fe/src/config/apiconfig.js
+++ fe/src/config/apiconfig.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL ='https://mcasms.onrender.com' || 'http://localhost:3001/api';
 
 const API_ENDPOINTS ={
     headers: `${API_BASE_URL}/headers`,
