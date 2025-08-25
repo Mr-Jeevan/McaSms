c564352b4dbcc3357f24f6e78364c0a2ea37f484
Mr-Jeevan <jeevee.a77@gmail.com>
2025-08-25 10:59:58 +0530

    giting

--- fe/src/layout/footer.jsx
+++ fe/src/layout/footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
     return (
         <section id='footer'>
             <footer className="footer mt-auto bg-one">
-                <div className="position-fixed bottom-0 start-0 end-0 mx-3">
+                <div className="position-fixed bottom-0 start-0 end-0 mx-5">
                     <div className="d-flex justify-content-between align-items-center">
                         <h2>SMS</h2>
                         <div>
-                            Developed by <a className="text-decoration-none footer-a" target='_blank' href="https://github.com/Mr-Jeevan/McaSms.git">Mr. Jeevan et al.,<i className="fa-regular fa-heart"></i></a>
+                            Developed by <a className=" footer-a" target='_blank' href="https://github.com/Mr-Jeevan/McaSms.git">Mr. Jeevan et al.,<i className="fa-regular fa-heart"></i></a>
                         </div>
                     </div>
                 </div>
